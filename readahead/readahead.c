6561a3dad828367d2ac4b3c6a9f44a4bef233d95
Jay Tu <arden.jay@gmail.com>
2018-06-14 16:59:10 +0800

    Adds readahead system call
    Signed-off-by: Jay Tu <arden.jay@gmail.com>

--- readahead/readahead.c
+++ readahead/readahead.c
@@ -3,6 +3,7 @@
 #include <fcntl.h>
 #include <string.h>
 #include <stdlib.h>
+#include <errno.h>
 
 struct readahead_request {
 	char* path;
@@ -15,8 +16,10 @@ void make_request_path(struct readahead_request* request, char* path)
 	size_t len = 0;
 
 	len = strlen(path);
-	if (request->path)
+	if (request->path) {
 		free(request->path);
+		request->path = NULL;
+	}
 	request->path = malloc(len);
 	strcpy(request->path, path);
 }
@@ -27,8 +30,10 @@ void make_request_off(struct readahead_request* request, char* ofs)
 	off64_t value = 0;
 	int i = 0;
 
-	if (request->offset)
+	if (request->offset) {
 		free(request->offset);
+		request->offset = NULL;
+	}
 	request->offset = malloc(sizeof(off64_t) * request->size);
 
 	token = strdup(ofs);
@@ -49,8 +54,28 @@ void dump(struct readahead_request* request)
 	printf("PATH - %s\n", request->path);
 	printf("Num: %d\n", request->size);
 	for (i = 0; i < request->size; i++)
-		printf("%ld ", request->offset[i]);
+		printf("%jd ", request->offset[i]);
 	printf("\n");
+}
+
+int make_request(struct readahead_request* request)
+{
+	int fd, i;
+	ssize_t ret;
+
+	fd = open(request->path, O_RDONLY);
+	if (fd < 0) {
+		printf("open %s: %s\n", request->path, strerror(errno));
+		goto fail;
+	}
+
+	for (i = 0; i < request->size; i++) {
+		ret = readahead(fd, request->offset[i], 1);
+		if (ret)
+			printf("%s fails: %s\n", __func__, strerror(errno));
+	}
+fail:
+	return -1;
 }
 
 int main(int argc, char **argv)
@@ -60,13 +85,22 @@ int main(int argc, char **argv)
 	ssize_t read;
 	FILE* fp;
 	int i = 0;
-	struct readahead_request* request = malloc(sizeof(struct readahead_request));
+	struct readahead_request* request;
+	char* readahead_list = argv[1];
 
-	fp = fopen("readahead_list", "r");
-	if (fp == NULL) {
-		printf("Can't open file\n");
+	if (argc < 2) {
+		printf("Use: readahead list\n");
 		exit(EXIT_FAILURE);
 	}
+
+	fp = fopen(readahead_list, "r");
+	if (fp == NULL) {
+		printf("Can't open readahead_list: %s\n", strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+
+	request = malloc(sizeof(struct readahead_request));
+	memset(request, 0, sizeof(*request));
 
 	while ((read = getline(&line, &len, fp)) != -1) {
 		dup = strdup(line);
@@ -84,8 +118,10 @@ int main(int argc, char **argv)
 			make_request_off(request, token);
 		}
 
-		if ((++i % 3) == 0)
+		if ((++i % 3) == 0) {
 			dump(request);
+			make_request(request);
+		}
 	}
 
 	fclose(fp);
